2a99109215b20f973c8f143f39b14c97bcd4cd14
R-Sev <rsevcik@outlook.cz>
2024-03-07 21:23:05 +0100

    get words count from text

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 
-def main():
-    with open(r"books/frankenstein.txt") as f:
-        content = f.read()
+def get_words_count(text):
+    return len(text.split())
 
+def get_book_text(path):
+    with open(path) as f:
+        content = f.read()
+        return content
+
+def main():
+    book_path = r"books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    words_count = get_words_count(book_text)
+    print(words_count)
 main()
