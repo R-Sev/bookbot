cded25512427b120108767caff27a06a7135704b
R-Sev <rsevcik@outlook.cz>
2024-03-07 22:10:10 +0100

    report generation

--- main.py
+++ main.py
@@ -19,10 +19,31 @@ def get_book_text(path):
     with open(path) as f:
         content = f.read()
         return content
+    
+def generate_report(path):
+    def sort_on(dict):
+        return dict["count"]
+
+    book_text = get_book_text(path)
+    words_count = get_words_count(book_text)
+    chars = char_occurrence(book_text)
+    list_of_chars = []
+    for ch in chars:
+        if ch.isalpha():
+            list_of_chars.append({"char": ch, "count": chars[ch]})
+    list_of_chars.sort(reverse=True, key=sort_on)
+
+    report = f"--- report of {path}"
+    report += f"\n{words_count} word found"
+    for ch in list_of_chars:
+        report += f"\nThe '{ch['char']}' was found {ch['count']} times"
+
+    report += "\n--- End of report ---"
+
+    return report
 
 def main():
     book_path = r"books/frankenstein.txt"
-    book_text = get_book_text(book_path)
-    words_count = get_words_count(book_text)
-    print(char_occurrence(book_text))
+    report = generate_report(book_path)
+    print(report)
 main()
